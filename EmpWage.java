806d9712b1215fa46cba9ca0d0594fa0bef6777f
NAZIYA8 <61536014+NAZIYA8@users.noreply.github.com>
2021-06-08 16:25:54 +0530

    Refactor
    Refactored :Calculate employee wage for multiple companies

--- EmpWage.java
+++ EmpWage.java
@@ -3,10 +3,7 @@ package com.bridgelabz;
 public class EmpWage {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
-    public static final int MAX_HRS_IN_MONTH = 10;
-    public static final int NUM_WORKING_DAYS = 2;
-    public static final int EMP_RATE_PER_HR = 20;
-
+   
     /**
      * Compute employee wage for multiple companies
      * @param company name of the company
